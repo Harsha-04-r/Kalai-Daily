27e5722c1276bdaa198d45b7f8c34cd1da0304b0
Harsha <harsharaj2007@gmail.com>
2026-06-23 14:06:59 +0530

    update database and requirements.txt to railway.app

--- main.py
+++ main.py
@@ -14,8 +14,13 @@ load_dotenv()
 app = Flask(__name__)
 
 app.secret_key = os.getenv('SECRET_KEY', 'fallback_dev_key')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///kalai.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+
+database_url = os.getenv("DATABASE_URL")
+if database_url:
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 
 db.init_app(app)
 login_manager.init_app(app)
